bc9375474a97c0b7d7b65ee7bf674b16453168dc
nv-h <saido.nv@gmail.com>
2024-03-27 20:40:35 +0900

    Update script

--- test/ble_from_pc.py
+++ test/ble_from_pc.py
@@ -2,41 +2,57 @@ import asyncio
 from bleak import BleakScanner
 from bleak import BleakClient
 from struct import unpack
+from datetime import datetime
+
+import argparse
 
 DEVICE_NAME = "nrf52_sensor"
 SERVICE_UUID = "c7839aa8-1903-40b5-a8f0-426e09ffb390"
 CHARACTERISTIC_UUID = "c7839aa9-1903-40b5-a8f0-426e09ffb390"
 
+sec = 10
+notify_count = 0
 
 def notify_handler(sender, data):
     unpacked = unpack("<ifffhh", data)
-    print(f"notify: {unpacked}")
+    now = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+    print(f"{now} {unpacked[0]}, {unpacked[1]:.2f}, {unpacked[2]:.2f}, {unpacked[3]:.2f}, {unpacked[4]}, {unpacked[5]}")
+
+    global notify_count
+    if notify_count % 120 == 0:
+        with open("sensor_data.csv", "a") as f:
+            f.write(f"{now}, {unpacked[0]}, {unpacked[1]:.2f}, {unpacked[2]:.2f}, {unpacked[3]:.2f}, {unpacked[4]}, {unpacked[5]}\n")
+
+    notify_count += 1
 
 
 async def main():
-    device = await BleakScanner.find_device_by_filter(
-        lambda d, ad: d.name and d.name.lower() == DEVICE_NAME.lower()
-    )
+    device = await BleakScanner.find_device_by_name(DEVICE_NAME)
     if not device:
         print(f"{DEVICE_NAME} not found")
         return
 
     print(f"Found device: {device.name} / {device.address}")
 
-    async with BleakClient(device, timeout=20.0) as client:
-        print("connected")
+    for i in range(10):
+        try:
+            async with BleakClient(device) as client:
+                print(f"time, battery, temperature, humidity, pressure, co2, tvoc")
+                with open("sensor_data.csv", "w") as f:
+                    f.write("time, battery, temperature, humidity, pressure, co2, tvoc\n")
+                await client.start_notify(CHARACTERISTIC_UUID, notify_handler)
+                await asyncio.sleep(sec)
+                await client.stop_notify(CHARACTERISTIC_UUID)
 
-        val = await client.read_gatt_char(CHARACTERISTIC_UUID)
-        print(f'read: {unpack("<ifffhh", val)}')
-
-        print('notify handler start')
-        await client.start_notify(CHARACTERISTIC_UUID, notify_handler)
-
-        await asyncio.sleep(10.0)
-
-        print('notify handler stop')
-        await client.stop_notify(CHARACTERISTIC_UUID)
+                break
+        except Exception as e:
+            print(e)
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--sec", type=int, default=10)
+    args = parser.parse_args()
+    sec = args.sec
+
     asyncio.run(main())
